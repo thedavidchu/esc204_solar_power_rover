e8ca10cf73e81e4b6854bc1e0cd7788ae6ee7f59
thedavidchu <thedavidchu@gmail.com>
2020-04-09 11:02:27 -0400

    Finalized

--- unloved functions.py
+++ unloved functions.py
@@ -17,3 +17,55 @@
                 print(i, ":", self.ans[min_product_index])
 
         return self.ans[min_product_index]
+
+    #####
+
+    def second_derivative(self,show=False):
+        """
+        Find the 'second derivative' of a discrete function by taking the second difference.
+
+        Returns a list of second derivatives.
+
+        X   X   X   X
+          Y   Y   Y
+            Z   Z
+        
+        """
+
+        df = [None]
+        d2f = [None]
+
+        # Take first difference: f1 = (f(x+dx)-f(x))/dx
+        for i in range(0,len(self.ans)-1,1):
+            df += [int((self.ans[i+1][1] - self.ans[i][1]) / (self.ans[i+1][0] - self.ans[i][0]))]
+
+        for i in range(1,len(self.ans)-1,1):
+            d2f += [int((df[i+1] - df[i]) / (self.ans[i+1][0] - self.ans[i][0]))]
+        d2f += [None]
+
+        if show == True:
+            print('First derivative:', df)
+            print('Second derivative:', d2f)
+
+        return d2f
+
+    def find_max_d2f(self,show=False):
+        d2f = self.second_derivative(show=show)
+        max_d2f = 0
+        max_index = 0
+        
+        for i in range(1,len(d2f)-1,1):
+            if d2f[i] > max_d2f:
+                max_d2f = d2f[i]
+                max_index = i
+
+        if show == True:
+            print(d2f,'\n\n',d2f[0:1000])
+            print('Largest Second Derivative:', max_index, self.ans[max_index], 'with d2f =', d2f[max_index])
+
+        return max_index
+
+    #####
+
+
+    
